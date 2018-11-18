a1aa6c9788884b1775a482f40d8993d178689060
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-11-18 02:30:27 -0600

    Add graceful ctrl-c exit
    Taken from https://github.com/svenstaro/memefs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate ctrlc;
 extern crate fuse;
 extern crate indexmap;
 extern crate libc;
@@ -5,6 +6,12 @@ extern crate rawr;
 extern crate time;
 
 use rawr::prelude::*;
+use std::sync::{
+  atomic::{AtomicBool, Ordering},
+  Arc,
+};
+use std::thread;
+use std::time::Duration;
 
 mod user;
 
@@ -18,7 +25,23 @@ fn main() {
     .collect::<Vec<_>>();
   let client = RedditClient::new(UA, AnonymousAuthenticator::new());
   let fs = user::UserFS::new(client);
-  if let Err(err) = fuse::mount(fs, &mountpoint, &options) {
-    println!("Error: {}", err);
+
+  let running = Arc::new(AtomicBool::new(true));
+  let r = running.clone();
+
+  ctrlc::set_handler(move || {
+    r.store(false, Ordering::SeqCst);
+  }).expect("Error setting Ctrl-C handler");
+
+  unsafe {
+    if let Some(str_mountpoint) = mountpoint.to_str() {
+      println!("Mounting to {}", str_mountpoint);
+    }
+    let _fuse_handle = fuse::spawn_mount(fs, &mountpoint, &options).unwrap();
+
+    while running.load(Ordering::SeqCst) {
+      thread::sleep(Duration::from_millis(100));
+    }
+    println!("Unmounting and exiting");
   }
 }
